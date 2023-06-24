e5bb9f6e9139e03ec27f8518949be4a1eb876987
QuantumWombat <jlconant14@gmail.com>
2023-06-24 13:12:32 -0700

    progress

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,9 @@
 """All magic numbers in a single file to make for easy tuning."""
 
-NUM_CARDS_TO_SHOW = 3
+NUM_STARTING_COMPANIONS = 3
+
+CARD_COST = 3
+COMPANION_COST = 4
+REROLL_COST = 1
+
+COMBAT_REWARD_GOLD = 5
--- src/entity.py
+++ src/entity.py
@@ -1,35 +1,13 @@
 """Module with entity information."""
 
 from enum import Enum
+import random
 from typing import List
 
 from src.card import Card
 
-class Entity:
-    def __init__(self, name: str, starting_health: int = 20, starting_attack: int = 1):
-        self.name = name
-        self.max_health = starting_health
-        self.health = starting_health
-        self.attack = starting_attack
-
-    def damage(self, amount: int):
-        self.health -= amount
-        if self.health <= 0:
-            print(f"Entity {self.name} died")
-
-
-class Enemy(Entity):
-    """Enemy represents a single enemy in Signified."""
-
-    def __init__(self, name: str, starting_health: int = 20, starting_attack: int = 1):
-        super().__init__(name, starting_health, starting_attack)
-
-    def __str__(self):
-        return f"😈 {self.name}: {self.health}/{self.max_health}💖"
-
-
-# The following enumerate the list of companions in Signified.
 class CompanionType(Enum):
+    """Static enumeration of all the companion types in Signified."""
     ARCHITECT = (
         "Architect",
         [Card.SUMMON_RAT],
@@ -48,6 +26,36 @@ class CompanionType(Enum):
         obj.starting_deck = starting_deck
         return obj
 
+    def generate_name(self) -> str:
+        prefix = random.choice(["WideLloyd", "Jimothy", "Foo", "Wizard", "Marmot"])
+        return prefix + self.value
+
+
+class Entity:
+    """Entity is the base class for all entity instances, friend or foe."""
+
+    def __init__(self, name: str, starting_health: int = 20, starting_attack: int = 1):
+        self.name = name
+        self.max_health = starting_health
+        self.health = starting_health
+        self.attack = starting_attack
+
+    def damage(self, amount: int):
+        self.health -= amount
+        print(self)
+        if self.health <= 0:
+            print(f"Entity {self.name} died")
+
+
+class Enemy(Entity):
+    """Enemy represents a single enemy in Signified."""
+
+    def __init__(self, name: str, starting_health: int = 20, starting_attack: int = 1):
+        super().__init__(name, starting_health, starting_attack)
+
+    def __str__(self):
+        return f"😈 {self.name}: {self.health}/{self.max_health}💖"
+
 
 class Companion(Entity):
     """Companion represents members of your team in Signified.
@@ -58,12 +66,13 @@ class Companion(Entity):
 
     def __init__(
         self,
+        name: str,
         companion_type: CompanionType,
         starting_health: int = 20,
         starting_attack: int = 1,
     ):
-        super().__init__(companion_type.value, starting_health, starting_attack)
-        self.starting_cards = companion_type.starting_deck
+        super().__init__(name, starting_health, starting_attack)
+        self.companion_type = companion_type
 
     def __str__(self):
         return f"{self.name}: {self.attack}⚔️ {self.health}/{self.max_health}💖"
